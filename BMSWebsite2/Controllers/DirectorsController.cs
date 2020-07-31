9b05b952d3a356f6a0c18afec9cae448055ced32
Vaishnave Subbramanian <vaishnave.subbramanian@psiog.com>
2020-07-31 15:10:24 +0530

    Added SQL connection in main app

---
+++ BMSWebsite2/Controllers/DirectorsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BMSDataAccess;
+
+namespace BMSWebsite2.Controllers
+{
+    public class DirectorsController : ApiController
+    {
+        public IEnumerable<DIRECTOR> Get()
+        {
+            using (BMSApplicationEntities entities = new BMSApplicationEntities())
+            {
+                return entities.DIRECTORs.Include("MOVIEs").ToList();
+            }
+        }
+
+        public DIRECTOR Get(int id)
+        {
+            using (BMSApplicationEntities entities = new BMSApplicationEntities())
+            {
+                return entities.DIRECTORs.FirstOrDefault(director => director.DirectorID==id);
+            }
+        }
+    }
+}
---
+++ BMSWebsite2/Controllers/MoviesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BMSDataAccess;
+
+namespace BMSWebsite2.Controllers
+{
+    public class MoviesController : ApiController
+    {
+
+        public IEnumerable<MOVIE> Get()
+        {
+            using (BMSApplicationEntities entities = new BMSApplicationEntities())
+            {
+                return entities.MOVIEs.ToList();
+            }
+        }
+
+        public MOVIE Get(int id)
+        {
+            using (BMSApplicationEntities entities = new BMSApplicationEntities())
+            {
+                return entities.MOVIEs.FirstOrDefault(m => m.MovieID == id);
+            }
+        }
+
+    }
+}
